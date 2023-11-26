a73f3621fd939d641ebb5aa9bc981424c6163171
a.muholzoev <amuk89@mail.ru>
2023-11-26 23:02:02 +0700

    towers destroing

--- Assets/Scripts/Tower/Tower5GHealth.cs
+++ Assets/Scripts/Tower/Tower5GHealth.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Main;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Tower5GHealth : MonoBehaviour, IVulnerable
 {
+    [SerializeField] private float _explosionTime;
+    [SerializeField] private GameObject _explosionCOntainer;
+    [SerializeField] private GameObject _view;
+    
     public int maxHealth = 100;
     private int currentHealth;
 
@@ -19,7 +26,17 @@ public class Tower5GHealth : MonoBehaviour, IVulnerable
 
         if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            DestroyProcess();
         }
+    }
+
+    private async void DestroyProcess()
+    {
+        gameObject.layer = LayerMask.NameToLayer("PhysicsIgnore");
+        _explosionCOntainer.gameObject.SetActive(true);
+        await Task.Delay(TimeSpan.FromSeconds(_explosionTime/2f));
+        _view.SetActive(false);
+        await Task.Delay(TimeSpan.FromSeconds(_explosionTime/2f));
+        Destroy(gameObject);
     }
 }
