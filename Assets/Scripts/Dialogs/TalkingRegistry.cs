f02b87c79448397d5892529c315dc4ae6b78ca9b
Andrey Muholzoev <amuk89@mail.ru>
2023-11-27 02:49:48 +0700

    imported

---
+++ Assets/Scripts/Dialogs/TalkingRegistry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dialogs
+{
+    [CreateAssetMenu(fileName = "TalkingRegistry", menuName = "Registries/TalkingRegistry", order = 0)]
+    public class TalkingRegistry : ScriptableObject
+    {
+        [SerializeField] private SpeechData _speeches;
+
+        public SpeechData Speeches => _speeches;
+    }
+
+    [Serializable]
+    public class SpeechData
+    {
+        [SerializeField] private string _idStoryteller;
+        [TextAreaAttribute][SerializeField] private string _speech;
+        [SerializeField] private AudioClip _audioClip;
+        [SerializeField] private float _timeOfTalk;
+
+        public string Speech => _speech;
+
+        public AudioClip Clip => _audioClip;
+
+        public float TimeOfTalk => _timeOfTalk;
+
+        public string IDStoryteller => _idStoryteller;
+    }
+}
